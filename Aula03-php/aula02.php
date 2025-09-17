ee2352fcca24554cf273ea668e63c81404447d98
Ana_Andreatta <ana.landreatta@aluno.edu.es.gov.br>
2025-09-17 11:36:11 -0300

    Create aula02.php

---
+++ Aula03-php/aula02.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Aula 02PHP</title>
+    </head>
+    <body>
+        <?php
+             $nome1="Luisa"
+             $nome2="Deus"
+
+             echo "Meu nome é $nome1 .";
+             echo "<br> Meu nome é $nome1 e minha dupla é $nome2";
+  
+        ?>
+    </body>
+</html>
