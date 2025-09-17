1d9e22c7e61b0409dcc699e0df65bdaad74ba2f4
Ana_Andreatta <ana.landreatta@aluno.edu.es.gov.br>
2025-09-17 11:57:32 -0300

    Create Aula04.php

---
+++ Aula03-php/Aula04.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Aula 04 PHP</title>
+    </head>
+    <body>
+        <?php
+            $n1=$_GET["a"];
+            $n2=$_GET["b"];
+
+            echo "a soma de $n1 e $n2 é: ".($n1+$n2);
+        ?>
+    </body>
+</html>
