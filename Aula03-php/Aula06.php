3eb0c25038bac865afb07f3d2b65307c8411ea32
Ana_Andreatta <ana.landreatta@aluno.edu.es.gov.br>
2025-10-01 08:46:01 -0300

    Create Aula06.php

---
+++ Aula03-php/Aula06.php
@@ -0,0 +1,20 @@
+<html>
+      <head>
+        <title>Aula 06 - PHP </title>
+    </head>
+
+    <body>
+        <?php
+            $a= 2;
+            $b=$a=5;
+
+           echo"A é igual a $a e B é igual a $b.".;
+           echo"<br><br>";
+           $a=$a+3;
+           echo"Agora A é igual a $a.";
+           echo"<br><br>";
+           $b=+8;
+           echo"Agora B é igual a $b.";
+           echo"<br><br>";
+           $b-=5;
+           echo"Agora B é igual a $b.";
